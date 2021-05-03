d6ea66930bb5c4445f066c07811cda1ada5a760e
Tanyiqu <1953649096@qq.com>
2021-05-03 08:01:09 +0800

    折线图

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <Pie />
+    <LineChart />
   </div>
 </template>
 
 <script>
-import Pie from './components/Pie.vue'
+import Pie from './components/Pie.vue';
+import LineChart from './components/Line.vue';
 
 export default {
   name: 'App',
   components: {
-    Pie
+    Pie, LineChart
   }
 }
 </script>
